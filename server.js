f0d557a01a0ff749104d65643708b7943efb012b
trungnd24 <40832789+trungnd24@users.noreply.github.com>
2020-04-11 14:45:58 +0700

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "successfully got users. Nice!",
+        user: mockUserData,
+    });
+});
+
 app.listen(8000, () => {
     console.log("server is running");
 });
